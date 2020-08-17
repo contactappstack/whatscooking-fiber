dade4ac50e4af127d2b6a987d97a16f6c3bd52cd
Raj-Madheshia <rajsmadheshia@gmail.com>
2020-08-17 17:32:09 +0530

    facebook login and firebase setup done.

--- src/sign-up/Login.js
+++ src/sign-up/Login.js
@@ -6,6 +6,7 @@ import SignUpContainer from "./SignUpContainer";
 import firebase from "firebase";
 import {TextField, Firebase} from "../components";
 import type {NavigationProps} from "../components/Types";
+import * as Facebook from 'expo-facebook';
 
 type LoginState = {
     loading: boolean
@@ -19,23 +20,27 @@ export default class Login extends React.Component<NavigationProps<*>, LoginStat
 
     @autobind
     async login(): Promise<void> {
-      try{
-        let { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('281875266000180', {
-            permissions: ['public_profile']
-        });
-        if (type === 'success') {
-        // Build Firebase credential with the Facebook access token.
-        const credential = firebase.auth.FacebookAuthProvider.credential(token);
-        firebase.auth().signInWithCredential(credential).catch((error) => {
-          // Handle Errors here.
-          alert("Enable to Signin !");
-        });
-        await AsyncStorage.setItem('fb_token', token);
-  }
-      }catch(e){
-        alert("Something went wrong! check your internet connectivity!")
-      }
+        try {
+            await Facebook.initializeAsync('304987363893598');
+            const { type, token } = await Facebook.logInWithReadPermissionsAsync({
+              permissions: ['public_profile'],
+            });
+            if (type === 'success') {
+                const credential = firebase.auth.FacebookAuthProvider.credential(token);
+                firebase.auth().signInWithCredential(credential).catch((error) => {
+                  // Handle Errors here.
+                  alert("Enable to Signin !");
+                });
+                await AsyncStorage.setItem('fb_token', token);
+            } else {
+              // type === 'cancel'
+              alert("Enable to Signin !");
+            }
+          } catch ({ message }) {
+            alert(`Facebook Login Error: ${message}`);
+          }
     }
+
     render(): React.Node {
         const {navigation} = this.props;
         const {loading} = this.state;
